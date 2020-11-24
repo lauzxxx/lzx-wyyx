79a0c80bfee53e3f5c3f123709b4fe13aa04c07a
lauzxxx <158741951@qq.com>
2020-11-24 21:59:52 +0800

    首页几乎完成

--- js/index.js
+++ js/index.js
@@ -32,3 +32,71 @@ for (var i = 0; i < topnav_else.length; i++) {
         life_list[this.index].style.display = 'none'
     }
 }
+
+//人气推荐选项卡切换
+$( '.edit-red' ).click( function(){
+    $( this ).addClass('small-checked')
+    $( '.hot-sold' ).removeClass('small-checked')
+    $( '.hot-ul-box-first' ).addClass('hot-ul-box-show')
+    $( '.hot-ul-box-second' ).removeClass('hot-ul-box-show')
+} );
+$( '.hot-sold' ).click( function(){
+    $( this ).addClass('small-checked')
+    $( '.edit-red' ).removeClass('small-checked')
+    $( '.hot-ul-box-second' ).addClass('hot-ul-box-show')
+    $( '.hot-ul-box-first' ).removeClass('hot-ul-box-show')
+} );
+
+//限时购倒计时
+
+(function show_tiem (){
+    var date = new Date()
+    var date_start = new Date().getTime()
+    $( '.screenHd span' ).text( date.getHours() )
+    var time = 3600000;
+    var timer = setInterval(function (){
+        time = time - 1000
+        if ( time > 0 ) {
+            var second = Math.floor (( time / 1000 )) % 60
+            var minite = Math.floor ( (time / 60000) ) % 60
+            var hour = Math.floor ( ( time / 3600000 ) ) % 24
+            if (second < 10) {
+                second = '0' + second
+            }
+            if (minite < 10) {
+                minite = '0' + minite
+            }
+            if (hour < 10) {
+                hour = '0' + hour
+            }
+            $( '.j-hour' ).text(hour)
+            $( '.j-min' ).text(minite)
+            $( '.j-second' ).text(second)
+        } else {
+            clearInterval( timer )
+        }
+    },1000)
+})()
+
+//轮播图
+var mySwiper = new Swiper ('.swiper-container', {
+    autoplay:true,
+    // direction: 'vertical', // 垂直切换选项
+    loop: true, // 循环模式选项
+    
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    // 如果需要滚动条
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  }) 
